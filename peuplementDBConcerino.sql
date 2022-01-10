b745a93d6dc6070952a35da3092a76b92349fd1b
Jonathan <chil.tfm@gmail.com>
2022-01-10 21:41:25 +0100

    fix: updateScript

--- peuplementDBConcerino.sql
+++ peuplementDBConcerino.sql
@@ -3,4 +3,4 @@ INSERT INTO utilisateur (login, nom, prénom, motdepasse, estmodérateur) VALUES (
 INSERT INTO utilisateur (login, nom, prénom, motdepasse, estmodérateur) VALUES ('b@gmail.com', 'Jane', 'Doe', '$2y$10$Tf6jDPjOEIhJKA6H6jyn6Oahpdhy7U97uJjYQF7MI07jdVMeWNMQi', FALSE);
 INSERT INTO utilisateur (login, nom, prénom, motdepasse, estmodérateur) VALUES ('c@gmail.com', 'Jonathan', 'Friedli', '$2y$10$Tf6jDPjOEIhJKA6H6jyn6Oahpdhy7U97uJjYQF7MI07jdVMeWNMQi', TRUE);
 INSERT INTO utilisateur (login, nom, prénom, motdepasse, estmodérateur) VALUES ('d@gmail.com', 'Stéphane', 'Marengo', '$2y$10$Tf6jDPjOEIhJKA6H6jyn6Oahpdhy7U97uJjYQF7MI07jdVMeWNMQi', TRUE);
-INSERT INTO utilisateur (login, nom, prénom, motdepasse, estmodérateur) VALUES ('d@gmail.com', 'Loris', 'Marzullo', '$2y$10$Tf6jDPjOEIhJKA6H6jyn6Oahpdhy7U97uJjYQF7MI07jdVMeWNMQi', TRUE);
+INSERT INTO utilisateur (login, nom, prénom, motdepasse, estmodérateur) VALUES ('e@gmail.com', 'Loris', 'Marzullo', '$2y$10$Tf6jDPjOEIhJKA6H6jyn6Oahpdhy7U97uJjYQF7MI07jdVMeWNMQi', TRUE);
