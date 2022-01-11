b88b0aca75b6ba89738d040c7bb09821a412ed8d
Jonathan <chil.tfm@gmail.com>
2022-01-11 13:24:32 +0100

    feat: display full user

--- src/app/views/error404.view.php
+++ src/app/views/error404.view.php
@@ -1,6 +1,7 @@
 <?php require('app/views/partials/header.php'); ?>
 <?php require('app/views/partials/nav.php'); ?>
 
-<h1 class="text-blue-400 text-center mt-24 text-5xl">Error 404 - Page not found</h1>
+<!-- <h1 class="text-blue-400 text-center mt-24 text-5xl">Error 404 - Page non trouvée</h1> -->
+<h1 class="text-blue-400 text-center mt-24 text-5xl">Désolé, cette page n'existe pas.</h1>
     
 <?php require('app/views/partials/footer.php'); ?>
--- src/app/views/users.view.php
+++ src/app/views/users.view.php
@@ -2,18 +2,11 @@
 <?php require('app/views/partials/nav.php'); ?>
 
 <h1 class="text-blue-400 text-center mt-24 text-5xl">Voici la liste des utilisateurs</h1>
-<h2 class="text-center text-lg text-blue-400">
-    <ul>
-        <?php foreach ($users as $user) : ?>
-            <li><?= $user->nom . ', ' . $user->prénom . ', ' . $user->login; ?></li>
-        <?php endforeach; ?>
-    </ul>
-</h2>
 
 <div class="flex flex-col items-center">
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8 max-w-2xl">
         <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-md sm:rounded-lg">
+            <div class="overflow-hidden shadow-lg sm:rounded-lg">
                 <table class="min-w-full">
                     <thead class="bg-gray-50">
                         <tr>
@@ -27,12 +20,12 @@
                                 Adresse mail
                             </th>
                             <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase">
-                                Est modo ?
+                                Rôle
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- Product 1 -->
+                        <!-- Product -->
                         <?php foreach ($users as $user) : ?>
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <td class="py-4 px-6 text-sm text-gray-400 whitespace-nowrap">
@@ -45,7 +38,11 @@
                                     <?= $user->login; ?>
                                 </td>
                                 <td class="py-4 px-6 text-sm text-gray-400 whitespace-nowrap">
-                                    <?= $user->estmodérateur; ?>
+                                    <?php if ($user->estmodérateur) : ?>
+                                        Modérateur
+                                    <?php else : ?>
+                                        Utilisateur
+                                    <?php endif; ?>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
