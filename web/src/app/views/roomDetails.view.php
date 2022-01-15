c1e0b67df21dbc67d919c7d25b2541d595f740f7
Marinlestylo <chil.tfm@gmail.com>
2022-01-15 11:41:22 +0100

    feat: all details for rooms

--- web/src/app/views/roomDetails.view.php
+++ web/src/app/views/roomDetails.view.php
@@ -5,4 +5,54 @@
     <?= $room[0]->nom ?>
 </h1>
 
+
+<div class="flex flex-col items-center mt-12">
+    <div class="max-w-4xl -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-blue-600">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
+                                Nom
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
+                                Capacité
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
+                                Adresse
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
+                                NPA et localité
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
+                                type
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <tr class="hover:bg-gray-100">
+                            <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap">
+                                <?= $room[0]->nom; ?>
+                            </td>
+                            <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap">
+                                <?= $room[0]->capacité ?> personnes
+                            </td>
+                            <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap">
+                                <?= $room[0]->nomrue . ' ' . $room[0]->norue ?>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-center text-gray-500 whitespace-nowrap">
+                                <?= $room[0]->npa . ' ' . $room[0]->localité ?>
+                            </td>
+                            <td class="px-6 py-4 text-sm text-center text-gray-500 whitespace-nowrap">
+                                <?= $room[0]->typelieu ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require('app/views/partials/footer.php'); ?>
--- web/src/app/views/rooms.view.php
+++ web/src/app/views/rooms.view.php
@@ -4,7 +4,7 @@
 <h1 class="mt-24 text-5xl text-center text-blue-400">Voici la liste des lieux de concert</h1>
 
 <div class="flex flex-col items-center mt-12">
-    <div class="max-w-4xl -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="max-w-6xl -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                 <table class="min-w-full divide-y divide-gray-200">
@@ -20,7 +20,7 @@
                                 Adresse
                             </th>
                             <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
-                                localité et NPA
+                                 NPA et localité
                             </th>
                             <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-blue-100 uppercase">
                                 type
@@ -30,17 +30,17 @@
                     <tbody class="bg-white divide-y divide-gray-200">
                         <?php foreach ($rooms as $room) : ?>
                             <tr class="hover:bg-gray-100">
-                                <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap">
-                                    <?= $room->nom ?>
+                                <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap hover:underline hover:text-black">
+                                <a href="/room?nom=<?= $room->nom ?>"><?= $room->nom; ?></a>
                                 </td>
                                 <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap">
-                                    <?= $room->capacité ?>
+                                    <?= $room->capacité ?> personnes
                                 </td>
                                 <td class="px-6 py-4 text-center text-gray-500 whitespace-nowrap">
                                     <?= $room->nomrue . ' ' . $room->norue ?>
                                 </td>
                                 <td class="px-6 py-4 text-sm text-center text-gray-500 whitespace-nowrap">
-                                    <?= $room->localité . ' ' . $room->npa ?>
+                                    <?= $room->npa . ' ' . $room->localité ?>
                                 </td>
                                 <td class="px-6 py-4 text-sm text-center text-gray-500 whitespace-nowrap">
                                     <?= $room->typelieu ?>
