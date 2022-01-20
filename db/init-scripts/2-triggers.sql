f7eb024e97b697f3cc690359bae3a4690359492c
BernardLhermite <stephane.marengo@hotmail.com>
2022-01-20 11:08:06 +0100

    trigger héritage

---
+++ db/init-scripts/2-triggers.sql
@@ -0,0 +1,49 @@
+/* ------------------------------------------------------------------ */
+-- Vérifie que le nouveau artiste solo n'est pas aussi un autre artiste
+
+CREATE OR REPLACE FUNCTION function_check_artiste_solo()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.id IN (SELECT id FROM Artiste) THEN
+        RAISE EXCEPTION 'Id d''artiste solo invalide --> %', NEW.id
+            USING HINT = 'L''heritage sur Artiste est disjoint. '
+                'Un artiste ne peut appartenir a plusieurs sous-types.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER check_artiste_solo
+    BEFORE INSERT
+    ON ArtisteSolo
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_artiste_solo();
+/* ------------------------------------------------------------------ */
+
+/* ------------------------------------------------------------------ */
+-- Vérifie que le nouveau groupe n'est pas aussi un autre artiste
+
+CREATE OR REPLACE FUNCTION function_check_groupe()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.id IN (SELECT id FROM Artiste) THEN
+        RAISE EXCEPTION 'Id de groupe invalide --> %', NEW.id
+            USING HINT = 'L''heritage sur Artiste est disjoint. '
+                'Un artiste ne peut appartenir a plusieurs sous-types.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER check_groupe
+    BEFORE INSERT
+    ON Groupe
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_groupe();
+/* ------------------------------------------------------------------ */
