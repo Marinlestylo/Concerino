1c7c2e973b220fd5a9892a07b8eb97da481f5ef3
Marinlestylo <chil.tfm@gmail.com>
2022-01-25 19:08:48 +0100

    fix: undo my shitty move

--- db/init-scripts/2-triggers.sql
+++ db/init-scripts/2-triggers.sql
@@ -1,55 +1,55 @@
--- /* ------------------------------------------------------------------ */
--- -- Vérifie que le nouveau artiste solo n'est pas aussi un autre artiste
+/* ------------------------------------------------------------------ */
+-- Vérifie que le nouveau artiste solo n'est pas aussi un autre artiste
 
--- CREATE OR REPLACE FUNCTION function_check_artiste_solo()
---     RETURNS TRIGGER AS
--- $$
--- BEGIN
---     IF NEW.id IN (SELECT id FROM Groupe) THEN
---         RAISE EXCEPTION 'Id d''artiste solo invalide --> %', NEW.id
---             USING HINT = 'L''heritage sur Artiste est disjoint. '
---                 'Un artiste ne peut appartenir a plusieurs sous-types.';
---     ELSE
---         RETURN NEW;
---     END IF;
--- END;
--- $$
---     LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION function_check_artiste_solo()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.id IN (SELECT id FROM Groupe) THEN
+        RAISE EXCEPTION 'Id d''artiste solo invalide --> %', NEW.id
+            USING HINT = 'L''heritage sur Artiste est disjoint. '
+                'Un artiste ne peut appartenir a plusieurs sous-types.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$
+    LANGUAGE plpgsql;
 
--- CREATE TRIGGER check_artiste_solo
---     BEFORE INSERT
---     ON ArtisteSolo
---     FOR EACH ROW
--- EXECUTE FUNCTION function_check_artiste_solo();
--- /* ------------------------------------------------------------------ */
+CREATE TRIGGER check_artiste_solo
+    BEFORE INSERT
+    ON ArtisteSolo
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_artiste_solo();
+/* ------------------------------------------------------------------ */
 
--- /* ------------------------------------------------------------------ */
--- -- Vérifie que le nouveau groupe n'est pas aussi un autre artiste
+/* ------------------------------------------------------------------ */
+-- Vérifie que le nouveau groupe n'est pas aussi un autre artiste
 
--- CREATE OR REPLACE FUNCTION function_check_groupe()
---     RETURNS TRIGGER AS
--- $$
--- BEGIN
---     IF NEW.id IN (SELECT id FROM ArtisteSolo) THEN
---         RAISE EXCEPTION 'Id de groupe invalide --> %', NEW.id
---             USING HINT = 'L''heritage sur Artiste est disjoint. '
---                 'Un artiste ne peut appartenir a plusieurs sous-types.';
---     ELSE
---         RETURN NEW;
---     END IF;
--- END;
--- $$
---     LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION function_check_groupe()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.id IN (SELECT id FROM ArtisteSolo) THEN
+        RAISE EXCEPTION 'Id de groupe invalide --> %', NEW.id
+            USING HINT = 'L''heritage sur Artiste est disjoint. '
+                'Un artiste ne peut appartenir a plusieurs sous-types.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$
+    LANGUAGE plpgsql;
 
--- CREATE TRIGGER check_groupe
---     BEFORE INSERT
---     ON Groupe
---     FOR EACH ROW
--- EXECUTE FUNCTION function_check_groupe();
--- /* ------------------------------------------------------------------ */
+CREATE TRIGGER check_groupe
+    BEFORE INSERT
+    ON Groupe
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_groupe();
+/* ------------------------------------------------------------------ */
 
--- /* ------------------------------------------------------------------ */
--- -- Plusieurs concerts ne peuvent pas avoir lieu en même temps et au même endroit
+/* ------------------------------------------------------------------ */
+-- Plusieurs concerts ne peuvent pas avoir lieu en même temps et au même endroit
 
 CREATE OR REPLACE FUNCTION function_check_concert_simultane_lieu()
     RETURNS TRIGGER AS
@@ -78,23 +78,23 @@ CREATE TRIGGER check_concert_simultane
 EXECUTE FUNCTION function_check_concert_simultane_lieu();
 /* ------------------------------------------------------------------ */
 
--- /* ------------------------------------------------------------------ */
--- -- Le nombre de personne inscrite à un concert ne peut pas excéder la capacité de sa salle.
+/* ------------------------------------------------------------------ */
+-- Le nombre de personne inscrite à un concert ne peut pas excéder la capacité de sa salle.
 
--- CREATE OR REPLACE FUNCTION function_check_concert_capacite_salle()
---     RETURNS TRIGGER AS
--- $$
--- DECLARE
---     participants INT;
---     capacite     INT;
--- BEGIN
---     SELECT COUNT(*), Lieu.capacité
---     INTO participants, capacite
---     FROM Utilisateur_Concert
---              INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
---              INNER JOIN Lieu ON Concert.nomLieu = Lieu.nom
---     WHERE Utilisateur_Concert.idConcert = NEW.idConcert
---     GROUP BY Lieu.nom;
+CREATE OR REPLACE FUNCTION function_check_concert_capacite_salle()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    participants INT;
+    capacite     INT;
+BEGIN
+    SELECT COUNT(*), Lieu.capacité
+    INTO participants, capacite
+    FROM Utilisateur_Concert
+             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
+             INNER JOIN Lieu ON Concert.nomLieu = Lieu.nom
+    WHERE Utilisateur_Concert.idConcert = NEW.idConcert
+    GROUP BY Lieu.nom;
 
     IF FOUND AND participants + 1 >= capacite THEN
         RAISE EXCEPTION 'Le concert % a atteint sa capacité maximale --> %', NEW.idConcert, participants;
