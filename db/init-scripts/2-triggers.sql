dd187b429f722a143df89f71ae152c18b7236a74
BernardLhermite <stephane.marengo@hotmail.com>
2022-01-27 18:27:54 +0100

    Refactor des triggers

--- db/init-scripts/2-triggers.sql
+++ db/init-scripts/2-triggers.sql
@@ -1,6 +1,7 @@
 /* ------------------------------------------------------------------ */
+/* Gestion de l'héritage Artiste - ArtisteSolo/Groupe                 */
+/* ------------------------------------------------------------------ */
 -- Vérifie que le nouveau artiste solo n'est pas aussi un autre artiste
-
 CREATE OR REPLACE FUNCTION function_check_artiste_solo()
     RETURNS TRIGGER AS
 $$
@@ -16,14 +17,6 @@ END;
 $$
     LANGUAGE plpgsql;
 
-CREATE TRIGGER check_artiste_solo
-    BEFORE INSERT
-    ON ArtisteSolo
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_artiste_solo();
-/* ------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------ */
 -- Vérifie que le nouveau groupe n'est pas aussi un autre artiste
 
 CREATE OR REPLACE FUNCTION function_check_groupe()
@@ -42,15 +35,22 @@ $$
     LANGUAGE plpgsql;
 
 CREATE TRIGGER check_groupe
-    BEFORE INSERT
+    BEFORE INSERT OR UPDATE
     ON Groupe
     FOR EACH ROW
 EXECUTE FUNCTION function_check_groupe();
+CREATE TRIGGER check_artiste_solo
+    BEFORE INSERT OR UPDATE
+    ON ArtisteSolo
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_artiste_solo();
 /* ------------------------------------------------------------------ */
 
 /* ------------------------------------------------------------------ */
+/* Concerts                                                           */
+/* ------------------------------------------------------------------ */
+
 -- Plusieurs concerts ne peuvent pas avoir lieu en même temps et au même endroit
-
 CREATE OR REPLACE FUNCTION function_check_concert_simultane_lieu()
     RETURNS TRIGGER AS
 $$
@@ -60,7 +60,8 @@ BEGIN
     SELECT nom
     INTO concertExistant
     FROM Concert
-    WHERE Concert.nomLieu = NEW.nomlieu
+    WHERE Concert.id != NEW.id
+      AND Concert.nomLieu = NEW.nomlieu
       AND NEW.début BETWEEN Concert.début AND (Concert.début + INTERVAL '1' MINUTE * Concert.durée);
     IF FOUND THEN
         RAISE EXCEPTION 'Un concert se déroule en même temps --> %', concertExistant
@@ -71,46 +72,34 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_concert_simultane
+-- Un concert ne peut pas être créé avec une date de début antérieure à la date courante.
+CREATE OR REPLACE FUNCTION function_check_concert_date_debut()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.début > NOW() THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'La date de début est dans le passé.';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_concert_date_debut
     BEFORE INSERT
+    ON Concert
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_concert_date_debut();
+
+CREATE TRIGGER check_concert_simultane
+    BEFORE INSERT OR UPDATE
     ON Concert
     FOR EACH ROW
 EXECUTE FUNCTION function_check_concert_simultane_lieu();
 /* ------------------------------------------------------------------ */
 
 /* ------------------------------------------------------------------ */
--- Le nombre de personne inscrite à un concert ne peut pas excéder la capacité de sa salle.
-
-CREATE OR REPLACE FUNCTION function_check_concert_capacite_salle()
-    RETURNS TRIGGER AS
-$$
-DECLARE
-    participants INT;
-    capacite     INT;
-BEGIN
-    SELECT COUNT(*), Lieu.capacité
-    INTO participants, capacite
-    FROM Utilisateur_Concert
-             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
-             INNER JOIN Lieu ON Concert.nomLieu = Lieu.nom
-    WHERE Utilisateur_Concert.idConcert = NEW.idConcert
-    GROUP BY Lieu.nom;
-
-    IF FOUND AND participants + 1 >= capacite THEN
-        RAISE EXCEPTION 'Le concert % a atteint sa capacité maximale --> %', NEW.idConcert, participants;
-    ELSE
-        RETURN NEW;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_concert_capacite_salle
-    BEFORE INSERT
-    ON Utilisateur_Concert
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_concert_capacite_salle();
-/* ------------------------------------------------------------------ */
-
+/* Concert_Artiste                                                    */
 /* ------------------------------------------------------------------ */
 -- Un artiste ne peut pas se produire lors de plusieurs concerts en même temps.
 
@@ -135,16 +124,90 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Le numéro de passage est une suite (commençant à 1) continue de valeurs.
+CREATE OR REPLACE FUNCTION function_check_concert_artiste_ordre()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    dernierNo SMALLINT;
+BEGIN
+    SELECT MAX(numéroPassage)
+    INTO dernierNo
+    FROM Concert_Artiste
+    WHERE idConcert = NEW.idConcert;
+    IF dernierNo + 1 != NEW.numéroPassage THEN
+        RAISE EXCEPTION 'Le numéro de passage doit être une suite continue de valeurs. Prochaine valeur : %', dernierNo + 1;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Un groupe sans membres actifs ne peut pas se produire en concert.
+CREATE OR REPLACE FUNCTION function_check_concert_groupe_actif()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    PERFORM
+    FROM Membre
+    WHERE idGroupe = NEW.idArtiste
+      AND dateFin IS NULL;
+
+    IF NOT FOUND AND NEW.idArtiste IN (SELECT id FROM Groupe) THEN
+        RAISE EXCEPTION 'Aucun membre actif dans le groupe.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_concert_groupe_actif
+    BEFORE INSERT OR UPDATE
+    ON Concert_Artiste
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_concert_groupe_actif();
+
+CREATE TRIGGER check_concert_artiste_ordre
+    BEFORE INSERT OR UPDATE --TODO DELETE
+    ON Concert_Artiste
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_concert_artiste_ordre();
+
 CREATE TRIGGER check_concert_artiste_simultane
-    BEFORE INSERT
+    BEFORE INSERT OR UPDATE
     ON Concert_Artiste
     FOR EACH ROW
 EXECUTE FUNCTION function_check_concert_simultane_artiste();
 /* ------------------------------------------------------------------ */
 
 /* ------------------------------------------------------------------ */
--- Un utilisateur ne peut pas s’inscrire à différents concerts ayant lieu en même temps.
+/* Utilisateur_Concert                                                */
+/* ------------------------------------------------------------------ */
+-- Le nombre de personne inscrite à un concert ne peut pas excéder la capacité de sa salle.
+CREATE OR REPLACE FUNCTION function_check_concert_capacite_salle()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    participants INT;
+    capacite     INT;
+BEGIN
+    SELECT COUNT(*), Lieu.capacité
+    INTO participants, capacite
+    FROM Utilisateur_Concert
+             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
+             INNER JOIN Lieu ON Concert.nomLieu = Lieu.nom
+    WHERE Utilisateur_Concert.idConcert = NEW.idConcert
+    GROUP BY Lieu.nom;
 
+    IF FOUND AND participants + 1 >= capacite THEN
+        RAISE EXCEPTION 'Le salle du concert % a atteint sa capacité maximale --> %', NEW.idConcert, participants;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Un utilisateur ne peut pas s’inscrire à différents concerts ayant lieu en même temps.
 CREATE OR REPLACE FUNCTION function_check_concert_simultane_utilisateur()
     RETURNS TRIGGER AS
 $$
@@ -167,188 +230,53 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER check_utilisateur_concert_simultane
-    BEFORE INSERT
+    BEFORE INSERT OR UPDATE
     ON Utilisateur_Concert
     FOR EACH ROW
 EXECUTE FUNCTION function_check_concert_simultane_utilisateur();
+
+CREATE TRIGGER check_concert_capacite_salle
+    BEFORE INSERT
+    ON Utilisateur_Concert
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_concert_capacite_salle();
 /* ------------------------------------------------------------------ */
 
 /* ------------------------------------------------------------------ */
--- Le numéro de passage est une suite (commençant à 1) continue de valeurs.
-
-CREATE OR REPLACE FUNCTION function_check_concert_artiste_ordre()
+/* Lieu                                                               */
+/* ------------------------------------------------------------------ */
+-- Le nombre de personne inscrite à un concert ne peut pas excéder la capacité de sa salle.
+CREATE OR REPLACE FUNCTION function_check_concert_changement_capacite_salle()
     RETURNS TRIGGER AS
 $$
-DECLARE
-    dernierNo SMALLINT;
 BEGIN
-    SELECT numéroPassage
-    INTO dernierNo
-    FROM Concert_Artiste
-    WHERE idConcert = NEW.idConcert
-    ORDER BY numéropassage DESC
-        FETCH FIRST 1 ROW ONLY;
-    IF dernierNo + 1 != NEW.numéroPassage THEN
-        RAISE EXCEPTION 'Le numéro de passage doit être une suite continue de valeurs. Prochaine valeur : %', dernierNo + 1;
+    PERFORM
+    FROM Utilisateur_Concert
+             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
+    WHERE nomLieu = NEW.nom
+      AND (début + INTERVAL '1' MINUTE * durée) > NOW()
+    GROUP BY id
+    HAVING COUNT(*) > NEW.capacité;
+
+    IF FOUND THEN
+        RAISE EXCEPTION 'Ce changement entraînerait l''invalidation de certains concerts.';
     ELSE
         RETURN NEW;
     END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_concert_artiste_ordre
-    BEFORE INSERT OR UPDATE
-    ON Concert_Artiste
+CREATE TRIGGER check_concert_changement_capacite_salle
+    BEFORE UPDATE
+    ON Lieu
     FOR EACH ROW
-EXECUTE FUNCTION function_check_concert_artiste_ordre();
+EXECUTE FUNCTION function_check_concert_changement_capacite_salle();
 /* ------------------------------------------------------------------ */
 
 /* ------------------------------------------------------------------ */
--- Un concert ne peut pas être créé avec une date de début antérieure à la date courante.
-
-CREATE OR REPLACE FUNCTION function_check_concert_date_debut()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    IF NEW.début > NOW() THEN
-        RETURN NEW;
-    ELSE
-        RAISE EXCEPTION 'La date de début est dans le passé.';
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_concert_date_debut
-    BEFORE INSERT OR UPDATE
-    ON Concert
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_concert_date_debut();
-/* ------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------ */
--- Un concert ne peut être noté que par un utilisateur ayant assisté audit concert.
-
-CREATE OR REPLACE FUNCTION function_check_note_concert()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.date = CURRENT_DATE;
-
-    PERFORM
-    FROM Utilisateur_Concert
-             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
-    WHERE idConcert = NEW.idConcert
-      AND idUtilisateur = NEW.idUtilisateur
-      AND début < NEW.date;
-
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'L''utilisateur n''a pas assisté à ce concert --> %', NEW.idConcert;
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_note_concert
-    BEFORE INSERT
-    ON NoteConcert
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_note_concert();
-/* ------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------ */
--- Une salle ne peut être notée que par un utilisateur ayant assisté à au moins un concert dans cette dernière.
-
-CREATE OR REPLACE FUNCTION function_check_note_lieu()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.date = CURRENT_DATE;
-
-    PERFORM
-    FROM Utilisateur_Concert
-             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
-    WHERE Concert.nomLieu = NEW.nom
-      AND idUtilisateur = NEW.idUtilisateur
-      AND Concert.début < NEW.date;
-
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'L''utilisateur n''a pas assisté à des concerts dans cette salle.';
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_note_lieu
-    BEFORE INSERT
-    ON NoteLieu
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_note_lieu();
-/* ------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------ */
--- Un artiste ne peut être noté que par un utilisateur ayant effectivement assisté à une de ses performances.
-
-CREATE OR REPLACE FUNCTION function_check_note_artiste()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    NEW.date = CURRENT_DATE;
-
-    PERFORM
-    FROM Utilisateur_Concert
-             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
-             INNER JOIN Concert_Artiste ON Concert.id = Concert_Artiste.idConcert
-    WHERE idUtilisateur = NEW.idUtilisateur
-      AND idArtiste = NEW.idArtiste
-      AND Concert.début < NEW.date;
-
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'L''utilisateur n''a pas assisté à des concerts de cet artiste.';
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_note_artiste
-    BEFORE INSERT
-    ON NoteArtiste
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_note_artiste();
-/* ------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------ */
--- Un groupe sans membres actifs ne peut pas se produire en concert.
-
-CREATE OR REPLACE FUNCTION function_check_concert_groupe_actif()
-    RETURNS TRIGGER AS
-$$
-BEGIN
-    PERFORM
-    FROM Membre
-    WHERE idGroupe = NEW.idArtiste
-      AND dateFin IS NULL;
-
-    IF NOT FOUND AND NEW.idArtiste IN (SELECT id FROM Groupe) THEN
-        RAISE EXCEPTION 'Aucun membre actif dans le groupe.';
-    ELSE
-        RETURN NEW;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER check_concert_groupe_actif
-    BEFORE INSERT
-    ON Concert_Artiste
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_concert_groupe_actif();
-/* ------------------------------------------------------------------ */
-
+/* Membre                                                             */
 /* ------------------------------------------------------------------ */
 -- La date de début d’un membre doit être antérieure ou égale à la date de fin.
-
 CREATE OR REPLACE FUNCTION function_check_date_membre()
     RETURNS TRIGGER AS
 $$
@@ -361,16 +289,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_date_membre
-    BEFORE INSERT OR UPDATE
-    ON Membre
-    FOR EACH ROW
-EXECUTE FUNCTION function_check_date_membre();
-/* ------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------ */
 -- Un artiste solo ne peut pas rejoindre un groupe dont il fait déjà partie.
-
 CREATE OR REPLACE FUNCTION function_check_membre_deja_groupe()
     RETURNS TRIGGER AS
 $$
@@ -393,4 +312,99 @@ CREATE TRIGGER check_membre_deja_groupe
     ON Membre
     FOR EACH ROW
 EXECUTE FUNCTION function_check_membre_deja_groupe();
+
+CREATE TRIGGER check_date_membre
+    BEFORE INSERT OR UPDATE
+    ON Membre
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_date_membre();
+/* ------------------------------------------------------------------ */
+
+/* ------------------------------------------------------------------ */
+/* Notes                                                              */
+/* ------------------------------------------------------------------ */
+-- Un concert ne peut être noté que par un utilisateur ayant assisté audit concert.
+CREATE OR REPLACE FUNCTION function_check_note_concert()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.date = CURRENT_DATE;
+
+    PERFORM
+    FROM Utilisateur_Concert
+             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
+    WHERE idConcert = NEW.idConcert
+      AND idUtilisateur = NEW.idUtilisateur
+      AND début < NEW.date;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'L''utilisateur n''a pas assisté à ce concert --> %', NEW.idConcert;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Une salle ne peut être notée que par un utilisateur ayant assisté à au moins un concert dans cette dernière.
+CREATE OR REPLACE FUNCTION function_check_note_lieu()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.date = CURRENT_DATE;
+
+    PERFORM
+    FROM Utilisateur_Concert
+             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
+    WHERE Concert.nomLieu = NEW.nom
+      AND idUtilisateur = NEW.idUtilisateur
+      AND Concert.début < NEW.date;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'L''utilisateur n''a pas assisté à des concerts dans cette salle.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Un artiste ne peut être noté que par un utilisateur ayant effectivement assisté à une de ses performances.
+CREATE OR REPLACE FUNCTION function_check_note_artiste()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.date = CURRENT_DATE;
+
+    PERFORM
+    FROM Utilisateur_Concert
+             INNER JOIN Concert ON Utilisateur_Concert.idConcert = Concert.id
+             INNER JOIN Concert_Artiste ON Concert.id = Concert_Artiste.idConcert
+    WHERE idUtilisateur = NEW.idUtilisateur
+      AND idArtiste = NEW.idArtiste
+      AND Concert.début < NEW.date;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'L''utilisateur n''a pas assisté à des concerts de cet artiste.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_note_concert
+    BEFORE INSERT OR UPDATE
+    ON NoteConcert
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_note_concert();
+
+CREATE TRIGGER check_note_lieu
+    BEFORE INSERT OR UPDATE
+    ON NoteLieu
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_note_lieu();
+
+CREATE TRIGGER check_note_artiste
+    BEFORE INSERT OR UPDATE
+    ON NoteArtiste
+    FOR EACH ROW
+EXECUTE FUNCTION function_check_note_artiste();
 /* ------------------------------------------------------------------ */
